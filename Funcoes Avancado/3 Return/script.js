b7a2e2682f9d1e6f55a4a8445999a961836bb422
Washington <lepexgamer102000@gmail.com>
2024-12-08 01:52:41 -0300

    feat: return de funções

--- Funcoes Avancado/3 Return/script.js
+++
@@ -1,7 +0,0 @@
-// return 
-// retorna um valor 
-// termina a função
-
-function soma(a, b){
-    return a + b;
-}
---
+++ Sec Funcoes Avancado/3 Return/script.js
@@ -0,0 +1,35 @@
+// return
+// retorna um valor
+// termina a função
+
+// function soma(a, b) {
+//   return a + b;
+// }
+// console.log(soma(4, 3));
+
+// document.addEventListener('click', function () {
+//   document.body.style.backgroundColor = 'purple';
+// });
+
+//Temos funções q retornam ou não retornam valor document por exemplo acima nao retorna, mas faz algo.
+
+// function criaPessoa(nome, sobrenm) {
+//   return { nome, sobrenm };
+// }
+
+// const p1 = criaPessoa('Luiz', 'Otávio');
+// const p2 = { nome: 'Luiz', sobrenm: 'Otávio' };
+
+// console.log(p1);
+// console.log(p2);
+
+// function falaFrase(comeco) {
+//   function falaResto(resto) {
+//     return comeco + ' ' + resto;
+//   }
+//   return falaResto;
+// }
+
+// const olaMundo = falaFrase('Olá');
+// console.log(olaMundo('Mundo'));
+// Onde esta boneca russa é util? No código abaixo é mostrado isso
