1f4d2e156e8c7c20f1f547a07658951c568f0153
Washington <lepexgamer102000@gmail.com>
2025-01-11 23:20:25 -0300

    feat: função geradora

---
+++ Sec Funcoes Avancado/12 Funcoes Geradoras/script.js
@@ -0,0 +1,60 @@
+/* function* geradora1() {
+  // Código qualquer
+  yield "Valor 1";
+  // Código qualquer
+  yield "Valor 2";
+  // Código qualquer
+  yield "Valor 3";
+}
+
+const g1 = geradora1();
+console.log(g1.next().value);
+console.log(g1.next().value);
+console.log(g1.next().value);
+console.log(g1.next());
+*/
+
+/* function* geradora1() {
+  yield "Valor 1";
+  yield "Valor 2";
+  yield "Valor 3";
+}
+
+const g1 = geradora1();
+console.log(g1.next().value);
+
+for (let valor of g1) {
+  console.log(valor);
+} */
+
+/* function* geradora2() {
+  let i = 0;
+
+  while (true) {
+    yield i;
+    i++;
+  }
+}
+
+const g2 = geradora2();
+console.log(g2.next().value); */
+
+function* geradora3() {
+  yield "Valor 0";
+  yield "Valor 1";
+  yield "Valor 2";
+}
+
+function* geradora4() {
+  yield* geradora3();
+  yield "Valor 3";
+  yield "Valor 4";
+  yield "Valor 5";
+}
+
+const g4 = geradora4();
+console.log(g4.next().value);
+for (let valor of g4) {
+  // for of é a mesma coisa que um foreach
+  console.log(valor);
+}
