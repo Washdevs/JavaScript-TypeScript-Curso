f1fa96da9c033a31ccc88a3ac462dcc35e3416c6
Washington <lepexgamer102000@gmail.com>
2025-01-15 21:25:23 -0300

    feat: Métodos para filtrar array

--- Sec 4 Array Avancado/2 Método Splice/script.js
+++ Sec 4 Array Avancado/2 Método Splice/script.js
@@ -1,8 +1,15 @@
+// const nome = ["Mancha", "Bruto", "King", "Cartucho", "Bala", "Cromo"];
+
+// console.log(nome[2]);
+
+// nome.splice(2, 1, "Cachorro", "Gato", "Papagaio");
+// //nome.splice(Posicao, Quantidade de elementos a serem removidos, Elementos a serem adicionados);
+// // acima para positivos e para negativos o mesmo conceito, mas a contagem é feita de trás para frente.
+// // Exemplo: nome.splice(-1, 1, "Cachorro", "Gato", "Papagaio"); // adiciona no final do array
+// console.log(nome);
+
 const nome = ["Mancha", "Bruto", "King", "Cartucho", "Bala", "Cromo"];
-
-console.log(nome[2]);
-
-nome.splice(2, 1, "Cachorro", "Gato", "Papagaio");
+nome.splice(-1, 1, "Cachorro", "Gato", "Papagaio");
 //nome.splice(Posicao, Quantidade de elementos a serem removidos, Elementos a serem adicionados);
 // acima para positivos e para negativos o mesmo conceito, mas a contagem é feita de trás para frente.
 // Exemplo: nome.splice(-1, 1, "Cachorro", "Gato", "Papagaio"); // adiciona no final do array
---
+++ Sec 4 Array Avancado/4 Filtrando Array/script.js
@@ -0,0 +1,15 @@
+// Comparando os métodos:
+// Método	Retorna	Usado Para
+// filter	Novo array	Filtrar elementos com base em uma condição.
+// map	Novo array	Transformar elementos do array.
+// reduce	Único valor	Agregar valores do array em um único resultado.
+// Combinando os métodos:
+// Os métodos podem ser combinados para tarefas mais complexas:
+
+const numeros = [1, 2, 3, 4, 5, 6];
+const resultado = numeros
+  .filter((num) => num % 2 === 0) // Filtra números pares
+  .map((num) => num * 2) // Dobra os pares
+  .reduce((soma, num) => soma + num, 0); // Soma os resultados
+
+console.log(resultado); // 20 (2*2 + 4*2 + 6*2)
