41f7908c4e0a3183c9894c09b8f278372723f219
Washington <lepexgamer102000@gmail.com>
2025-01-23 23:30:54 -0300

    feat: compreendendo as funções object.defineproperty e properties.

--- Sec 6 Objetos e Prototypes Avancado/2 Func defineProperty/script.js
+++ Sec 6 Objetos e Prototypes Avancado/2 Func defineProperty/script.js
@@ -10,12 +10,6 @@ function Produto(nome, marca, preco) {
   });
 }
 
-<<<<<<< HEAD
 const p1 = new Produto("Camiseta", 20);
 p1.preco = 25; // preço não é alterado por causa do writable
 console.log(Object.keys(p1));
-=======
-const p1 = new Produto("Camisa", "Lost", 120);
-console.log(p1);
-console.log(`O produto: ${p1.nome} da marca: ${p1.marca} custa ${p1.preco}`);
->>>>>>> c807d952b843497baafef4e03433b2decac0f1a5
