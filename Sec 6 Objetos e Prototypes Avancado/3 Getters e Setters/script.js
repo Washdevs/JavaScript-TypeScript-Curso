3281926f74883e20421800f267ff88550ab7861d
Washington <lepexgamer102000@gmail.com>
2025-01-26 23:09:30 -0300

    feat: Trabalhando com get e set em um defineProperties

--- Sec 6 Objetos e Prototypes Avancado/3 Getters e Setters/script.js
+++ Sec 6 Objetos e Prototypes Avancado/3 Getters e Setters/script.js
@@ -13,19 +13,36 @@ function Produto(nome, preco) {
       enumerable: true,
       configurable: false,
       get: function () {
+        // O trabalho do get é so pegar o valor no momento e exibir ele
         return preco;
       },
       set: function () {
-        if (typeof this.preco === "number") {
-          preco = this.preco;
-        } else {
-          console.log("preço não pode ser string");
+        // O trabalho do set é validar o valor
+        if (typeof this.preco != "number") {
+          throw new TypeError("O tipo deve ser um número");
         }
       }
     }
   });
 }
 
-const p1 = new Produto("Camiseta", "22");
-p1.preco = 25; // preço não é alterado por causa do writable
-console.log(p1.nome, p1.preco);
+// Segundo exemplo manipulando o objeto com a função construtora
+function criaProduto() {
+  return {
+    get nome() {
+      return nome;
+    },
+    set nome(valor) {
+      valor = valor.replace("coisa", "");
+      nome = valor;
+    }
+  };
+}
+
+// const p1 = new Produto("Camiseta", 22);
+// p1.preco = 25; // preço não é alterado por causa do writable
+// console.log(p1.nome, p1.preco);
+
+const p2 = criaProduto("Camiseta");
+p2.nome = "Qualquer coisa";
+console.log(p2.nome);
