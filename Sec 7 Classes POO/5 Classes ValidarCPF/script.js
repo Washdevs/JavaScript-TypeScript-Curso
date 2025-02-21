e208d51728d1e86e1cb0141c5e3fdbec9c63c75d
Washington <lepexgamer102000@gmail.com>
2025-02-21 00:29:30 -0300

    feat: Validação de CPF

--- Sec 7 Classes POO/5 Classes ValidarCPF/script.js
+++ Sec 7 Classes POO/5 Classes ValidarCPF/script.js
@@ -1,3 +1,4 @@
+//Lógica do Professor
 class ValidaCPF {
   constructor(cpfEnviado) {
     Object.defineProperty(this, "cpfLimpo", {
@@ -8,7 +9,24 @@ class ValidaCPF {
     });
   }
 
-  geraNovoCpf() {}
+  geraNovoCpf() {
+    const cpfsemDigito = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.geraDigito(cpfsemDigito);
+    const digito2 = this.geraDigito(cpfsemDigito + digito1);
+    this.NovoCpf = cpfsemDigito + digito1 + digito2;
+  }
+
+  geraDigito(cpfsemDigito) {
+    let total = 0;
+    let reverso = cpfsemDigito.length + 1;
+    for (let Stringnumero of cpfsemDigito) {
+      // console.log(Stringnumero, reverso);
+      total += reverso * Number(Stringnumero);
+      reverso--;
+    }
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : "0";
+  }
 
   validaSequencia() {
     return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo; //se cpfLimpo na posição 0 se repete 11 vezes. Se isto for igual a cpfLimpo é true
@@ -19,10 +37,15 @@ class ValidaCPF {
     if (typeof this.cpfLimpo !== "string") return "Não é String";
     if (this.cpfLimpo.length !== 11) return "Não tem 11 digitos";
     if (this.validaSequencia()) return "Não Pode Ser sequencia";
-    if (!this.geraNovoCpf()) return "Não gerou novo CPF";
-    return "Valido";
+    if (this.geraNovoCpf()) return "Não gerou novo CPF";
+    return this.NovoCpf;
   }
 }
 //070.987.720-03
 const cpf = new ValidaCPF("070.987.720-03");
-console.log(cpf.validacpf());
+
+if (cpf.validacpf()) {
+  console.log("CPF Válido");
+} else {
+  console.log("CPF Inválido");
+}
