21541e723b50191b1437d9157fb020254370f92c
Washington <lepexgamer102000@gmail.com>
2025-02-23 23:31:17 -0300

    feat: Concluido Validação de CPF baseada em Metodos de uma Classe

--- Sec 7 Classes POO/5 Classes ValidarCPF/script.js
+++ Sec 7 Classes POO/5 Classes ValidarCPF/script.js
@@ -9,14 +9,23 @@ class ValidaCPF {
     });
   }
 
+  validacpf() {
+    if (!this.cpfLimpo) return "Não é Limpo";
+    if (typeof this.cpfLimpo !== "string") return "Não é String";
+    if (this.cpfLimpo.length !== 11) return "Não tem 11 digitos";
+    if (this.validaSequencia()) return "Não Pode Ser sequencia";
+    if (this.geraNovoCpf()) return "Não gerou novo CPF";
+    return this.NovoCpf;
+  }
+
   geraNovoCpf() {
     const cpfsemDigito = this.cpfLimpo.slice(0, -2);
-    const digito1 = this.geraDigito(cpfsemDigito);
-    const digito2 = this.geraDigito(cpfsemDigito + digito1);
+    const digito1 = ValidaCPF.geraDigito(cpfsemDigito);
+    const digito2 = ValidaCPF.geraDigito(cpfsemDigito + digito1); //This é a instancia, mudamos para classe pq o metodo é static e não depende de this.
     this.NovoCpf = cpfsemDigito + digito1 + digito2;
   }
 
-  geraDigito(cpfsemDigito) {
+  static geraDigito(cpfsemDigito) {
     let total = 0;
     let reverso = cpfsemDigito.length + 1;
     for (let Stringnumero of cpfsemDigito) {
@@ -31,15 +40,6 @@ class ValidaCPF {
   validaSequencia() {
     return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo; //se cpfLimpo na posição 0 se repete 11 vezes. Se isto for igual a cpfLimpo é true
   } // === não é para receber igual é = ele serve para comparar, cpf limpo se repete 11 vezes igual é a operação atrás? se true a função valida vai dar return Não pode ser sequencia
-
-  validacpf() {
-    if (!this.cpfLimpo) return "Não é Limpo";
-    if (typeof this.cpfLimpo !== "string") return "Não é String";
-    if (this.cpfLimpo.length !== 11) return "Não tem 11 digitos";
-    if (this.validaSequencia()) return "Não Pode Ser sequencia";
-    if (this.geraNovoCpf()) return "Não gerou novo CPF";
-    return this.NovoCpf;
-  }
 }
 //070.987.720-03
 const cpf = new ValidaCPF("070.987.720-03");
