bfb2b4ba6d56f93bf2b3b8b0a32bf6e5e2a75966
Washington <lepexgamer102000@gmail.com>
2025-03-11 23:13:55 -0300

    feat: Pegando o jeito de Promisse para AsyncAwait

--- Sec 8 JS Assincrono/3 AsyncAwait/script.js
+++ Sec 8 JS Assincrono/3 AsyncAwait/script.js
@@ -1,12 +1,25 @@
+function rand(min = 0, max = 2) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject) => {
-    //oque diz se ela é uma promessa
-    if (typeof msg !== "string") {
-      reject(false);
-      return;
-    }
     setTimeout(() => {
-      resolve(msg.toUpperCase());
+      if (typeof msg !== "string") {
+        reject("Valor inválido");
+        return;
+      }
+      resolve(msg.toUpperCase() + "- Passei na Promisse".toUpperCase());
+      return;
     }, tempo);
   });
 }
+
+esperaAi("fase1", rand()).then((valor) => {
+  console.log(valor);
+  return esperaAi("fase2", rand()).then((valor) => {
+    console.log(valor);
+  });
+});
